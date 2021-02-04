b372f187aef62387c6a9a368d6c38995a40d1825
a <a>
2021-02-04 14:47:38 +0100

    oupsi

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 
 
 def main():
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier('files/haarcascade_frontalface_default.xml')
     cap = cv2.VideoCapture(0)
 
     while True:
