039fec9f570d354db9910a49403ff0d0f3c4b9c0
a <a>
2021-02-12 10:45:39 +0100

    Fct : Version finale ou quasi

--- src/main.py
+++ src/main.py
@@ -1,38 +1,41 @@
 import cv2
+import argparse
 
-face_cascade = cv2.CascadeClassifier('../files/haarcascade_frontalface_default.xml')
-specs_ori = cv2.imread('../files/lunettes.png', -1)
-cigar_ori = cv2.imread('../files/joint.png', -1)
-
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FPS, 30)
+parser = argparse.ArgumentParser(description='Augmented reality application')
+parser.add_argument('-s', '--source', help='Determine the source for the video', default=0, type=str)
+args = parser.parse_args()
 
 
 def main():
+    glasses_names = ['glasses_01.png', 'glasses_02.png', 'glasses_03.png']
+    glasses = []
+    glasses_choice = 0
+    for glasses_name in glasses_names:
+        glasses.append(cv2.imread('../files/' + glasses_name, -1))
+
+    face_cascade = cv2.CascadeClassifier('../files/haarcascade_frontalface_default.xml')
+    glasses_ori = glasses[glasses_choice]
+    cigar_ori = cv2.imread('../files/joint.png', -1)
+
+    cap = cv2.VideoCapture(args.source)
+    cap.set(cv2.CAP_PROP_FPS, 30)
+
     while True:
-        ret, video = cap.read()
-        faces = face_cascade.detectMultiScale(video, 1.2, 5, 0, (120, 120), (350, 350))
-        for (x, y, w, h) in faces:
-            glass_symin = int(y + 1.5 * h / 5)
-            glass_symax = int(y + 2.5 * h / 5)
-            sh_glass = glass_symax - glass_symin
+        ret, frame = cap.read()
 
-            cigar_symin = int(y + 4 * h / 6)
-            cigar_symax = int(y + 5.5 * h / 6)
-            sh_cigar = cigar_symax - cigar_symin
+        faces = face_cascade.detectMultiScale(frame, 1.2, 5, 0)
+        frame = insert_images_on_faces(faces, frame, glasses_ori, cigar_ori)
 
-            face_glass_roi_color = video[glass_symin:glass_symax, x:x + w]
-            face_cigar_roi_color = video[cigar_symin:cigar_symax, x:x + w]
+        cv2.imshow('Video', frame)
+        key = cv2.waitKey(30)
 
-            specs = cv2.resize(specs_ori, (w, sh_glass), interpolation=cv2.INTER_CUBIC)
-            cigar = cv2.resize(cigar_ori, (int(w / 2), sh_cigar), interpolation=cv2.INTER_CUBIC)
-            transparent_overlay(face_glass_roi_color, specs)
-            transparent_overlay(face_cigar_roi_color, cigar, (int(w / 2), int(sh_cigar / 5)))
-
-        cv2.imshow('Video', video)
-
-        if cv2.waitKey(30) & 0xff == 27:
+        if key == 113:
             break
+        elif key == 83:
+            glasses_choice += 1
+            if glasses_choice > len(glasses) - 1:
+                glasses_choice = 0
+            glasses_ori = glasses[glasses_choice]
 
     cap.release()
     cv2.destroyAllWindows()
@@ -51,6 +54,27 @@ def transparent_overlay(src, overlay, pos=(0, 0), scale=1):
             alpha = float(overlay[i][j][3] / 255.0)  # read the alpha channel
             src[x + i][y + j] = alpha * overlay[i][j][:3] + (1 - alpha) * src[x + i][y + j]
     return src
+
+
+def insert_images_on_faces(faces, frame, glasses, cigar):
+    for (x, y, w, h) in faces:
+        glass_symin = int(y + 1.5 * h / 5)
+        glass_symax = int(y + 2.5 * h / 5)
+        sh_glass = glass_symax - glass_symin
+
+        cigar_symin = int(y + 4 * h / 6)
+        cigar_symax = int(y + 5.5 * h / 6)
+        sh_cigar = cigar_symax - cigar_symin
+
+        face_glass_roi_color = frame[glass_symin:glass_symax, x:x + w]
+        face_cigar_roi_color = frame[cigar_symin:cigar_symax, x:x + w]
+
+        specs = cv2.resize(glasses, (w, sh_glass), interpolation=cv2.INTER_CUBIC)
+        cigar = cv2.resize(cigar, (int(w / 2), sh_cigar), interpolation=cv2.INTER_CUBIC)
+        transparent_overlay(face_glass_roi_color, specs)
+        transparent_overlay(face_cigar_roi_color, cigar, (int(w / 2), int(sh_cigar / 5)))
+
+    return frame
 
 
 if __name__ == '__main__':
