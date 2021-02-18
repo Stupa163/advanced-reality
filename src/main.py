d860cfa999069ef97dfd763ba8ea272c7b905da9
a <a>
2021-02-18 10:14:09 +0100

    Fct : filtre de iench

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Augmented reality application')
 parser.add_argument('-s', '--source', help='Determine the source for the video', default=0, type=str)
+parser.add_argument('-d', '--dog', help='Display a mask with a dog mask', action='store_true')
 args = parser.parse_args()
 
 
@@ -10,6 +11,7 @@ def main():
     glasses_names = ['glasses_01.png', 'glasses_02.png', 'glasses_03.png']
     glasses = []
     glasses_choice = 0
+    dog = cv2.imread('../files/dog.png')
     for glasses_name in glasses_names:
         glasses.append(cv2.imread('../files/' + glasses_name, -1))
 
@@ -24,7 +26,15 @@ def main():
         ret, frame = cap.read()
 
         faces = face_cascade.detectMultiScale(frame, 1.2, 5, 0)
-        frame = insert_images_on_faces(faces, frame, glasses_ori, cigar_ori)
+
+        if args.dog:
+            for x, y, w, h in faces:
+                y0, y1 = int(y - 0.25 * h), int(y + 0.75 * h)
+                x0, x1 = x, x + w
+
+                frame[y0: y1, x0: x1] = insert_dog_face_on_faces(frame[y0: y1, x0: x1], dog)
+        else:
+            frame = insert_images_on_faces(faces, frame, glasses_ori, cigar_ori)
 
         cv2.imshow('Video', frame)
         key = cv2.waitKey(30)
@@ -75,6 +85,26 @@ def insert_images_on_faces(faces, frame, glasses, cigar):
         transparent_overlay(face_cigar_roi_color, cigar, (int(w / 2), int(sh_cigar / 5)))
 
     return frame
+
+
+def insert_dog_face_on_faces(face, mask):
+    """Add the mask to the provided face, and return the face with mask."""
+    mask_h, mask_w, _ = mask.shape
+    face_h, face_w, _ = face.shape
+
+    factor = min(face_h / mask_h, face_w / mask_w)
+    new_mask_w = int(factor * mask_w)
+    new_mask_h = int(factor * mask_h)
+    new_mask_shape = (new_mask_w, new_mask_h)
+    resized_mask = cv2.resize(mask, new_mask_shape)
+
+    face_with_mask = face.copy()
+    non_white_pixels = (resized_mask < 250).all(axis=2)
+    off_h = int((face_h - new_mask_h) / 2)
+    off_w = int((face_w - new_mask_w) / 2)
+    face_with_mask[off_h: off_h + new_mask_h, off_w: off_w + new_mask_w][non_white_pixels] = resized_mask[non_white_pixels]
+
+    return face_with_mask
 
 
 if __name__ == '__main__':
